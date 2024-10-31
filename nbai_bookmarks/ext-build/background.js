b878d2ef440a9afa2fda9fbba8a25a3a6c60782c
Jim Welch <j.gauntlett.welch@gmail.com>
2024-10-31 08:23:20 -0400

    add lambda for synthetic data creation for demos. update chrome extension to create sample RO problems that get copied to clipboard add one NEMT example to bookmarks

--- nbai_bookmarks/ext-build/background.js
+++ nbai_bookmarks/ext-build/background.js
@@ -1,3 +1,12 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed and ready");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getUserInfo") {
+    chrome.identity.getProfileUserInfo((userInfo) => {
+      if (userInfo && userInfo.email) {
+        sendResponse({ email: userInfo.email });
+      } else {
+        sendResponse({ email: null });
+      }
+    });
+    return true; // Keep the message channel open for asynchronous response
+  }
 });
